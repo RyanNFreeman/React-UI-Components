1f58ef952633eef5d1aaa1db6262bfe543f05121
RyanNFreeman <ryan.norman.freeman@gmail.com>
2019-02-11 16:51:56 -0700

    mvp done. Should have committed more. Will adjust some sytling.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent'
+import HeaderTitle from './HeaderTitle'
+import HeaderThumbnail from './ImageThumbnail'
+
+
+const HeaderContainer = () => {
+    return (
+        <div className='header-container'>
+            <HeaderThumbnail/>
+            <div className='head'>
+            <HeaderTitle/>
+            <HeaderContent/>
+            </div>
+        </div>
+    );
+};
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderThumbnail = () => {
+    return (
+        <img className='logo' src='https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png' alt='logo'/>
+    );
+};
+
+export default HeaderThumbnail;
